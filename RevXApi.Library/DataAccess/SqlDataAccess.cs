84fe504046ed8050ee92355568bf7131ced4e07b
Israel Huss <israelmhuss@gmail.com>
2021-03-11 11:04:13 -0500

    Code cleanup

--- RevXApi.Library/DataAccess/SqlDataAccess.cs
+++ RevXApi.Library/DataAccess/SqlDataAccess.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Dapper;
 
 namespace RevXApi.Library.DataAccess
 {
