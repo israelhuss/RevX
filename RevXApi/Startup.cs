348b9cffffb27dfcd667188b8013a7f11a3695b8
Israel Huss <israelmhuss@gmail.com>
2021-03-18 12:28:40 -0400

    Add Api Project, With Jwt and Swagger

---
+++ RevXApi/Startup.cs
@@ -0,0 +1,106 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RevXApi.Data;
+using System;
+using System.Text;
+
+namespace RevXApi
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("RevXApiAuthDb")));
+			services.AddDatabaseDeveloperPageExceptionFilter();
+
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			services.AddControllersWithViews();
+
+			// JWT Authentication
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = "JwtBearer";
+				options.DefaultChallengeScheme = "JwtBearer";
+			})
+				.AddJwtBearer("JwtBearer", jwtBearerOptions =>
+				{
+					jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySECreTKeYIsSEcReTSoDONtTeLl")),
+						ValidateIssuer = false,
+						ValidateAudience = false,
+						ValidateLifetime = true,
+						ClockSkew = TimeSpan.FromMinutes(5)
+					};
+				});
+
+			// Swagger
+			services.AddSwaggerGen(setup =>
+			{
+				setup.SwaggerDoc(
+				"v1",
+				new OpenApiInfo
+				{
+					Title = "RevX API",
+					Version = "v1"
+				});
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseSwagger();
+			app.UseSwaggerUI(x =>
+			{
+				x.SwaggerEndpoint("/swagger/v1/swagger.json", "RevX API v1");
+			});
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapRazorPages();
+			});
+		}
+	}
+}
