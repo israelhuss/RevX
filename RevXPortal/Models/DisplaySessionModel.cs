39e14983e7fdb0a1d2d0ad70a210d157bc99029b
israelmhuss <israelmhuss@gmail.com>
2021-03-23 12:23:54 -0400

    Add sessions table

---
+++ RevXPortal/Models/DisplaySessionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevXPortal.Models
+{
+	public class DisplaySessionModel
+	{
+		public string Student { get; set; }
+		public DateTime Date { get; set; }
+		public string StartTime { get; set; }
+		public string EndTime { get; set; }
+		public string Provider { get; set; }
+		public string BillingStatus { get; set; }
+	}
+}
---
+++ RevXPortal/Models/SessionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RevXPortal.Models
+{
+	public class SessionModel
+	{
+		public string Student { get; set; }
+		public DateTime Date { get; set; }
+		public TimeSpan StartTime { get; set; }
+		public TimeSpan EndTime { get; set; }
+		public string Provider { get; set; }
+		public string BillingStatus { get; set; }
+		public string Notes { get; set; }
+	}
+}
