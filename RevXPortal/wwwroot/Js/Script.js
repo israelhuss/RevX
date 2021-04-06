1582c32aed82438c0b16f4ba6ce272fc8b693979
israelmhuss <israelmhuss@gmail.com>
2021-04-06 12:15:27 -0400

    Add TimeInput Control

---
+++ RevXPortal/wwwroot/Js/Script.js
@@ -0,0 +1,15 @@
+﻿var focused;
+window.isFocused = (element) => {
+    focused = element;
+};
+
+window.ContainerClicked = (element) => {
+    console.log(focused, element.children, document.activeElement.classList);
+    if (focused != undefined && focused.classList.contains("time-input")) {
+        return true;
+    } else {
+        return false;
+    }
+
+    //element.focus();
+};
