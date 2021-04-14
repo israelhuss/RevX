f940ff7dcbc1d65a43376bfe01963a4e79240696
israelmhuss <israelmhuss@gmail.com>
2021-04-14 12:29:08 -0400

    Invoice Template fun :(

--- RevXPortal/Services/PdfService.cs
+++ RevXPortal/Services/PdfService.cs
@@ -23,12 +23,49 @@ namespace RevXPortal.Services
 			//string path = Directory.GetCurrentDirectory();
 			//Console.WriteLine(Directory.GetCurrentDirectory());
 			//HtmlConverter.ConvertToPdf(html, new FileStream(file, FileMode.Create));
+			MemoryStream ms = new();
+			PdfWriter writer = new PdfWriter(ms);
+			PdfDocument pdf = new(writer);
+			HtmlConverter.ConvertToPdf(html, writer);
 
+			var x = pdf.GetDocumentInfo();
 			using (FileStream pdfDest = File.Open("output.pdf", FileMode.OpenOrCreate))
 			{
 				ConverterProperties converterProperties = new ConverterProperties();
 				HtmlConverter.ConvertToPdf(html, pdfDest, converterProperties);
 			}
+
+			//// create the HTML to PDF converter
+			//HtmlToPdf htmlToPdfConverter = new HtmlToPdf();
+			//// set PDF page size and orientation
+			//htmlToPdfConverter.Document.PageSize = GetSelectedPageSize();
+			//htmlToPdfConverter.Document.PageOrientation = GetSelectedPageOrientation();
+			//// set PDF page margins
+			//htmlToPdfConverter.Document.Margins = new PdfMargins(0);
+			//// convert HTML to 
+			//PDFbyte[] pdfBuffer = null;
+			//if (radioButtonConvertUrl.Checked)
+			//{
+			//	// convert URL to a PDF memory buffer
+			//	string url = textBoxUrl.Text;
+			//	pdfBuffer = htmlToPdfConverter.ConvertUrlToMemory(url);
+			//}
+			//else
+			//{
+			//	// convert HTML code    
+			//	string htmlCode = textBoxHtmlCode.Text;
+			//	string baseUrl = textBoxBaseUrl.Text;
+			//	// convert HTML code to a PDF memory buffer    
+			//	pdfBuffer = htmlToPdfConverter.ConvertHtmlToMemory(htmlCode, baseUrl);
+			//}
+			//// inform the browser about the binary data 
+			//formatHttpContext.Current.Response.AddHeader("Content-Type", "application/pdf");
+			//// set how to open the PDF document, attachment or inline, and the file name
+			//HttpContext.Current.Response.AddHeader("Content-Disposition", String.Format("{0}; filename=HtmlToPdf.pdf; size={1}", checkBoxOpenInline.Checked ? "inline" : "attachment", pdfBuffer.Length.ToString()));
+			//// write the PDF buffer to HTTP response
+			//HttpContext.Current.Response.BinaryWrite(pdfBuffer);
+			//// call End() method of HTTP response to stop ASP.NET page processing
+			//HttpContext.Current.Response.End();
 		}
 	}
 }
